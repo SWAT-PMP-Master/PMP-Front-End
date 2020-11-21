24a8677b60b3ff1426a66c0994577ba0fa3f5eea
Juan Guerrero <jcamiloguerrero@gmail.com>
2020-11-21 14:40:20 -0500

    update/add auth home material module and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    loadChildren: () => import ('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import ('./auth/auth.module').then(m => m.AuthModule)
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules, useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
