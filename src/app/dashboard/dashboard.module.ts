ed80c21863c7509b9167a3431fa3de96539d554c
Juan Guerrero <jcamiloguerrero@gmail.com>
2020-12-04 23:08:39 -0500

    fix/ nav dashboard and header router link update/404

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -5,13 +5,22 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { NavComponent } from './components/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout'
+
+import { MaterialModule } from './../material/material.module';
 
 
 @NgModule({
-  declarations: [DashboardComponent, ProfileComponent, NavComponent],
+  declarations: [
+    DashboardComponent,
+    ProfileComponent,
+    NavComponent
+  ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    LayoutModule,
+    MaterialModule
   ]
 })
 export class DashboardModule { }
