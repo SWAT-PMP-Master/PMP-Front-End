fe1bb1fc3c75a79dcdf94c9da8d1a3b0cef64558
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-12-07 14:38:30 -0500

    divide and make the diferentes home layouts. by RedKamo

---
+++ src/app/home/components/description-section/description-section.model.ts
@@ -0,0 +1,4 @@
+export interface Icon {
+  image: string;
+  description: string;
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { DescriptionSectionComponent } from './components/description-section/description-section.component';
+import { OurTeamComponent } from './components/our-team/our-team.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, BannerComponent, DescriptionSectionComponent, OurTeamComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
