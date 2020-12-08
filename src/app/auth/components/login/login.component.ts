ee25ee0e19cbcf3df8ecb6d186055ed958b9b787
cesar puentes <caps2695@gmail.com>
2020-12-07 20:05:43 -0500

    connection with trello

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,15 +1,29 @@
+import { core } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+
+declare var Trello;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  loginTrello(): void {
+    Trello.authorize({
+      type: 'popup',
+      name: 'Getting Started Application',
+      scope: {
+        read: 'true',
+        write: 'true',
+      },
+      expiration: 'never',
+      success: console.log('Successful authentication'),
+      error: console.log('Failed authentication'),
+    });
   }
-
 }
